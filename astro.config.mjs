36dabe9fc23691876868f274274ad9eade7f983a
xintao0202 <xintao0202@gmail.com>
2026-05-06 22:14:21 -0700

    space removal

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://inspiresaplingai.github.io',
-  integrations: [tailwind()],
+    site: 'https://inspiresaplingai.github.io',
+    integrations: [tailwind()],
 });
